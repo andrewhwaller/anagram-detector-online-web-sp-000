cb078e8d3333ddd3e61038496376a9fdbe1831ca
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 04:18:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,7 +17,7 @@ class Anagram
     #4. if some_word.split(" ") == word.split(" ") then @@matches << some_word return @@matches
     some_words.each do |possible_match|
       if possible_match.split(" ") == word.split(" ")
-        then @@matches << some_words
+        then @@matches.push
       end
     end
     @@matches
