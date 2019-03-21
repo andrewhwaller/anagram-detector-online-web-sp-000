afc2f6120770eb9c8c6ae8380e6d55ec6367f730
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 04:25:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,6 +13,6 @@ class Anagram
     #2. split into characters
     #3. split word into characters and compare
     #4. if some_word.split(" ") == word.split(" ") then @@matches << some_word return @@matches
-    array.select {|w| w.split(" ").sort == @name.split(" ").sort}
+    array.select {|w| w.split("").sort == @name.split("").sort}
   end
 end
