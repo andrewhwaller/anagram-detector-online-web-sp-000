95cd9e3e04c1b0788d3e4fc4965204ebc4861b62
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 04:13:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,7 +15,7 @@ class Anagram
     #2. split into characters
     #3. split word into characters and compare
     #4. if some_word.split(" ") == word.split(" ") then @@matches << some_word return @@matches
-    some_words.each do |some_word| if some_word.split(" ") == word.split(" ") then @@matches << some_word
+    some_words.each {|some_word| if some_word.split(" ") == word.split(" ") then @@matches << some_word end }
     end
     @@matches
   end
